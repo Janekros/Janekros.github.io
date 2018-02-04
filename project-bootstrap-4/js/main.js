82ac90e8fb6dfd297a3d8edcd4bdaa1afb17f46b
Janekros <janekrosochacki@gmail.com>
2018-02-04 13:04:03 +0100

    progressbar
    początek

--- project-bootstrap-4/js/main.js
+++ project-bootstrap-4/js/main.js
@@ -1,9 +1,25 @@
 $('a[href*="http"]').click(function(e){ //selektor oznacza, że wybieramy link, który w atrybucie href zawiera ciąg znaków http
     e.preventDefault();
-    console.log('test');
+    
     var url = $(this).attr('href'); //pobieramy wartość atrybutu href dla 'klikniętego linka
-    console.log(url);
+    
     $('body').fadeOut(5000, function() {
         window.location.href = url; // zamienia nam dla aktualnego okna adres aby wyświetliło stronę, która jest zawarta w klikniętym linku
     });
+});
+
+$(window).scroll(function() {
+   console.log($(this).scrollTop()); //sprawdza jak daleko od góry stronu 'odscrollowaliśmy'
+    
+    if($(this).scrollTop()>70) {
+        $('.navbar').addClass('scroll');
+    } else {
+        $('.navbar').removeClass('scroll');
+    }
+});
+
+$(window).scroll(function() {
+   var progressBarWidth = ($(this).scrollTop() * 100)/($('body').height() - $(window).height());
+    console.log(progressBarWidth);
+    
 });
